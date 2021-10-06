277aa53aa5f397ba822f7317263bcd2946d80643
rijal <jeryzal333@gmail.com>
2021-10-07 04:45:32 +0700

    Header component finish and show in landing pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "assets/scss/style.scss";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPages";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello wordl</h1>
+      <Router>
+        <Route exact path="/" component={LandingPage}></Route>
+      </Router>
     </div>
   );
 }
